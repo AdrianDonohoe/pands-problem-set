928af7013f4bc073125231d5daa2a5d1c5c032fd
Adrian Donohoe <allezlesblues1@gmail.com>
2019-02-07 13:52:59 +0000

    First Draft with basic structure, Logic to be added

--- solution-1.py
+++ solution-1.py
@@ -1,1 +1,30 @@
 # Solution to 2019 problem 1
+
+# Task
+# Write a program that takes any positive integer and outputs the sum of all numbers
+# between 1 and that number.
+# $ python sumupto.py
+#  Please enter a positive integer: 10
+# 55
+
+# declare variables needed
+i = 0
+ans = 0
+
+# Request user to input a postive integer
+# learned this from https://stackoverflow.com/questions/70797/user-input-and-command-line-arguments
+
+i = input("Please enter a positive integer: ")
+
+# TBD
+#  Test is positive, test is string and ask again if doesnt fit criteria
+print("You entered ", i )
+
+# TBD
+# Do the arithmetic
+
+# Print the answer
+
+print("The sum of all numbers between 1 and ", i , "is :")
+
+print(ans)
