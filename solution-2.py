1c4e67f09e0e229845ed05f3e1a063911209c76f
Adrian Donohoe <allezlesblues1@gmail.com>
2019-02-08 10:16:10 +0000

    Created solution 2 empty file

---
+++ solution-2.py
@@ -0,0 +1,11 @@
+# Solution to 2019 problem 2
+
+# Task
+# Write a program that outputs whether or not today is a day that begins with the
+# letter T. An example of running this program on a Thursday is as follows.
+# $ python begins-with-t.py
+# Yes - today begins with a T.
+
+# An example of running it on a Wednesday is as follows.
+# $ python begins-with-t.py
+# No - today does not begin with a T.
