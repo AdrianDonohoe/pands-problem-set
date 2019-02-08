a4b72172ed6e1d70b6593bea5111d557a93f15f6
Adrian Donohoe <allezlesblues1@gmail.com>
2019-02-08 13:36:02 +0000

    dictionary created

--- solution-2.py
+++ solution-2.py
@@ -9,3 +9,19 @@
 # An example of running it on a Wednesday is as follows.
 # $ python begins-with-t.py
 # No - today does not begin with a T.
+
+# we learned this in an earlier example
+import datetime
+
+# assign day of the week in number to tday Mon=0, Tues=1 ....
+tday = datetime.datetime.today().weekday()
+
+# print(tday)
+
+# Create a dictionary of days of the week
+alldays = { 0: 'Monday', 1: 'Tuesday', 2 : 'Wednesday', 3: 'Thursday', 4 : 'Friday', 5 : 'Saturday', 6 : 'Sunday'}
+
+
+
+
+
