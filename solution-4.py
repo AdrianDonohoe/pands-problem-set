d09b73b5cdce24356d2a6026bb1ae74e3a687bf3
Adrian Donohoe <allezlesblues1@gmail.com>
2019-02-15 10:54:59 +0000

    logic added

--- solution-4.py
+++ solution-4.py
@@ -1,7 +1,7 @@
 # Solution to 2019 problem 4
 
 # Task
-# Write a program that asks the user ot input any positive integer and outputs the
+# Write a program that asks the user to input any positive integer and outputs the
 # successive values of the following calculation performed on it up to and including
 # when the value becomes 1. The calculation is: if the number is even, divide it by
 # two, if it is odd then multiply it by three and add one.
@@ -9,3 +9,18 @@
 # $ python collatz.py
 # Please enter a positive integer: 10
 # 10 5 16 8 4 2 1
+
+num = input("Please enter any positive integer: ")
+
+while not num.isnumeric() or int(num) < 0 :      # re-used code from solution 1
+    num = input("Please enter any positive integer: ")
+
+num = int(num)
+
+while num != 1:
+    if num % 2 == 0:
+        num /= 2
+        print(int(num))
+    else :
+        num = num * 3 + 1
+        print(int(num))
