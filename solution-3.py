2898650e08b125de83213dcb9fff36c5047487e8
Adrian Donohoe <allezlesblues1@gmail.com>
2019-02-15 09:37:50 +0000

    Created solution 3 empty file

---
+++ solution-3.py
@@ -0,0 +1,12 @@
+# Solution to 2019 problem 3
+
+# Task
+# Write a program that prints all numbers between 1,000 and 10,000 that are divisible
+# by 6 but not 12.
+
+# $ python divisors.py
+# 1002
+# 1014
+# 1026
+# etc
+# 9990
