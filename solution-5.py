2ed7836b3a21308232454779f49d1a5adeb061df
Adrian Donohoe <allezlesblues1@gmail.com>
2019-02-15 11:50:57 +0000

    logic added

--- solution-5.py
+++ solution-5.py
@@ -8,3 +8,19 @@
 # Please enter a positive integer: 19
 # That is a prime.
 
+num = input("Please enter any positive integer: ")
+
+while not num.isnumeric() or int(num) < 0 :      # re-used code from solution 1, check if a positive number
+    num = input("Please enter any positive integer: ")
+
+num = int(num)
+
+if num == 1 :     # 1 is not prime and logic below can't test that so have added this statement
+    print(num, "is not prime")
+else:
+    for i in range(2,num):
+        if num % i == 0:
+            print(num , "is not prime")
+            break
+    else :
+        print(num , "is prime")
