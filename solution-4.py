75963a88c740563561368789287cbb1bea9e4bf3
Adrian Donohoe <allezlesblues1@gmail.com>
2019-02-15 10:27:49 +0000

    Created solution 4 empty file

---
+++ solution-4.py
@@ -0,0 +1,11 @@
+# Solution to 2019 problem 4
+
+# Task
+# Write a program that asks the user ot input any positive integer and outputs the
+# successive values of the following calculation performed on it up to and including
+# when the value becomes 1. The calculation is: if the number is even, divide it by
+# two, if it is odd then multiply it by three and add one.
+
+# $ python collatz.py
+# Please enter a positive integer: 10
+# 10 5 16 8 4 2 1
