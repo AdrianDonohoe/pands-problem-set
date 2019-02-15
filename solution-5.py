2f58b9e70f070d488720fec08f0469e25e8f8381
Adrian Donohoe <allezlesblues1@gmail.com>
2019-02-15 11:09:47 +0000

    Created solution 5 empty file

---
+++ solution-5.py
@@ -0,0 +1,10 @@
+# Solution to 2019 problem 5
+
+# Task
+
+# Write a program that asks the user to input a positive integer and tells the user
+# whether or not the number is a prime.
+# $ python primes.py
+# Please enter a positive integer: 19
+# That is a prime.
+
