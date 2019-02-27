38c24f47633596bb49d8ea5e8de4c166986f9606
Adrian Donohoe <allezlesblues1@gmail.com>
2019-02-27 14:57:22 +0000

    Created solution 8 empty file

---
+++ solution-8.py
@@ -0,0 +1,9 @@
+# Created by Adrian Donohoe 27th February 2019
+# Solution to 2019 problem 8
+
+# Task
+
+# Write a program that outputs today’s date and time in the format ”Monday, January
+# 10th 2019 at 1:15pm”.
+# $ python datetime.py
+# Monday, January 10th 2019 at 1:15pm
