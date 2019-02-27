5cdae8ffe331cc685d7ddcb2e49c992e8e96cd6a
Adrian Donohoe <allezlesblues1@gmail.com>
2019-02-27 13:47:11 +0000

    Created solution 6 empty file

---
+++ solution-6.py
@@ -0,0 +1,9 @@
+# Created by Adrian Donohoe 27th February 2019
+# Solution to 2019 problem 6
+
+# Task
+
+# Write a program that takes a user input string and outputs every second word.
+# $ python secondstring.py
+# Please enter a sentence: The quick brown fox jumps over the lazy dog.
+# The brown jumps the dog
