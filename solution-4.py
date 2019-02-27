e1c4d11a504c24b294ee85b42f480aae29434dee
Adrian Donohoe <allezlesblues1@gmail.com>
2019-02-27 14:48:31 +0000

    Added capability to remove some punctuation

--- solution-4.py
+++ solution-4.py
@@ -17,12 +17,12 @@ while not num.isnumeric() or int(num) < 0 :      # Adapted code from solution 1,
     num = input("Please enter any positive integer: ")
 
 num = int(num)  # make num an integer as input() returns a string
-print(num)
+print(num, end=' ') # added end so that prints on same line
 
 while num != 1:   # while num is not equal to one
     if num % 2 == 0:     # if num is even
         num /= 2      # num is assigned num divided by 2, equivalent to num = num / 2
-        print(int(num))  # for some reason floats were getting printed
+        print(int(num), end=' ')  # for some reason floats were getting printed
     else :    # else ifnum is odd 
         num = num * 3 + 1  # num is assigned num multiplied by 3 and add 1, then print
-        print(int(num))  # for some reason floats were getting printed
+        print(int(num), end=' ')  # for some reason floats were getting printed
--- solution-6.py
+++ solution-6.py
@@ -11,8 +11,12 @@
 # Request user to input a string, split it and assign to list 
 s1 = input("Please enter a sentence: ")
 
-# split string into list  , adapted from https://docs.python.org/3/library/stdtypes.html?highlight=str%20split#str.split
+# adapted from https://stackoverflow.com/a/3559600
+s1 = s1.replace(',','') # remove comma's
+s1 = s1.replace('.','') # remove full stops
+
+# split string into list, adapted from https://docs.python.org/3/library/stdtypes.html?highlight=str%20split#str.split
 l = s1.split()
 
-for i in range(0,len(l),2):  # loop over the list and print
-    print(l[i], end=' ')
+for i in range(0,len(l),2):  # loop from start to length of list in steps of 2 
+    print(l[i], end=' ')     # and print list element i with space 
