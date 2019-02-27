f2765c9fd1246dfc8e69ab1abd8e801942ce20e4
Adrian Donohoe <allezlesblues1@gmail.com>
2019-02-27 14:17:06 +0000

    Initial basic splitting and print

--- solution-6.py
+++ solution-6.py
@@ -7,3 +7,12 @@
 # $ python secondstring.py
 # Please enter a sentence: The quick brown fox jumps over the lazy dog.
 # The brown jumps the dog
+
+# Request user to input a string, split it and assign to list 
+s1 = input("Please enter a sentence: ")
+
+# split string into list  , adapted from https://docs.python.org/3/library/stdtypes.html?highlight=str%20split#str.split
+l = s1.split()
+
+for i in range(len(l)):  # loop over the list and print
+    print(l[i], end=' ')
