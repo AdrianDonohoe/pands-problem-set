100bfbe4601ce1ac0e2f95603a49fc07095b8c30
Adrian Donohoe <allezlesblues1@gmail.com>
2019-02-28 15:55:54 +0000

    Created solution 7 empty file

---
+++ solution-7.py
@@ -0,0 +1,10 @@
+# Created by Adrian Donohoe 28th February 2019
+# Solution to 2019 problem 7
+
+# Task
+
+# Write a program that that takes a positive floating point number as input and outputs
+#an approximation of its square root.
+# $ python squareroot.py
+# Please enter a positive number: 14.5
+# The square root of 14.5 is approx. 3.8.
