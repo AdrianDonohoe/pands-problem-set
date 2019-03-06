959b5ae642582c2871c4be43f263885be486be5a
Adrian Donohoe <allezlesblues1@gmail.com>
2019-03-06 13:46:54 +0000

    comments added

--- solution-9.py
+++ solution-9.py
@@ -5,8 +5,8 @@
 
 # Write a program that reads in a text file and outputs every second line.
 
-with open('numbers.txt','r') as f:
-    l = list(f)
+with open('numbers.txt','r') as f:   # open file as f. covered in week 7 video.
+    l = list(f)   # create a list from file. Adapted from https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
-for i in range(0,len(l),2):
-    print(l[i],end='')
+for i in range(0,len(l),2): # loop over the list in steps of 2.
+    print(l[i],end='')   # print, with end ='' to stop the newline
