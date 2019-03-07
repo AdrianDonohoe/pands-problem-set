0fb35ef32a829bb3c2a37991091ef5c63a22cbfd
Adrian Donohoe <allezlesblues1@gmail.com>
2019-03-07 14:31:40 +0000

    using argv and added moby-dick.txt

--- solution-9.py
+++ solution-9.py
@@ -4,8 +4,16 @@
 # Task
 
 # Write a program that reads in a text file and outputs every second line.
+# The program should take the filename from an argument on the command line.
+# $ python second.py moby-dick.txt
+# Title: Moby Dick; or The Whale
+# CHAPTER 1
+# Call me Ishmael. Some years ago--never mind how long
+# particular to interest me on shore, I thought I would sail about a
 
-with open('numbers.txt','r') as f:   # open file as f. covered in week 7 video.
+import sys # if we want to pass arguments using argv we must import sys module. Adapted from python tutorial https://docs.python.org/3/tutorial/interpreter.html#argument-passing
+
+with open(sys.argv[1],'r') as f:   # open file as f. covered in week 7 video. Must use argv[1] as the filename is the second argumemt after the python command
     l = list(f)   # create a list from file. Adapted from https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 for i in range(0,len(l),2): # loop over the list in steps of 2.
