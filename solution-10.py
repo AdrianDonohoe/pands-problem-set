584c51792a8aa53fb01600b172658276e5f9f2d3
Adrian Donohoe <allezlesblues1@gmail.com>
2019-03-27 10:53:37 +0000

    corrected plot 2x -> 2**x

--- solution-10.py
+++ solution-10.py
@@ -10,12 +10,12 @@ import matplotlib.pyplot as plot
 
 x = np.array([0,1,2,3,4])   # make an array for the x-axis
 plot.xlabel('x')   # label the x-axis, adapted from https://matplotlib.org/users/pyplot_tutorial.html
-plot.ylabel('f(x) = x, x**2, 2x')  # label the y-axis adapted from https://matplotlib.org/users/pyplot_tutorial.html
+plot.ylabel('f(x) = x, x**2, 2**x')  # label the y-axis adapted from https://matplotlib.org/users/pyplot_tutorial.html
 plot.title('Problem 10')   # Give title to plots, adapted from https://matplotlib.org/users/pyplot_tutorial.html
 
 xsquared = x**2   # squares the x-axis range for f(x)=x**2
 
-twox = x*2  # multiplies the x-axis range by two
+twotothex = 2**x  # multiplies the x-axis range by two
 
-plot.plot(x,x,'b-',x,xsquared,'g^',x,twox,'r--') # plot the 3 functions with blue line, green triangle and red dashes
+plot.plot(x,x,'b-',x,xsquared,'g^',x,twotothex,'r--') # plot the 3 functions with blue line, green triangle and red dashes
 plot.show() # show the plot
